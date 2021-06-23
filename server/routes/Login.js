04800aad3b0bfe038aef8ebfe51e4dca5b8333f6
“[Gracy <[gracyspatel@outlook.com]>
2021-06-23 15:30:14 +0530

    login connection

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -3,37 +3,32 @@ import UserModel from "../models/UserSchema.js";
 import bcrypt from "bcrypt"
 const app = express();
 app.use(json());
-const users=[
-    {
-        userEmail:"bc@gmail.com",
-        userPassword:"$2b$10$IfENkGn3z/.PaoWMh4qJs.QtC1gL7Fbumn4gxRc.zAQ0.FKNfxGtW"
-    }
-]
+//Trial
+// const users=[
+//     {
+//         userEmail:"bc@gmail.com",
+//         userPassword:"$2b$10$IfENkGn3z/.PaoWMh4qJs.QtC1gL7Fbumn4gxRc.zAQ0.FKNfxGtW"
+//     }
+// ]
 const LoginUser = async(req, res) => {
     const { userEmail, userPassword } = req.body;
-    // res.send(req.body)
-    //user => user.userEmail = req.body.userEmail
-    console.log(UserModel)
-    const user = UserModel.find({userEmail:req.body.userEmail})
-    console.log(user)
-    if(user == null)
+  UserModel.findOne(
     {
-         return res.status(400).send('Cannot find user')
-    }
-    return res.status(400).send('user found')
-    try{
-        if (await bcrypt.compare(req.body.userPassword,user.userPassword)) {
-            res.send('Success')
-            console.log('suc')
+      userEmail: userEmail,
+    },
+    (err, docs) => {
+      if (err) console.log(err);
+      else {
+        if (docs) {
+          if (bcrypt.compare(userPassword, docs["userPassword"])) {
+            res.send('Succesfully loged In');
+          } else res.send("WRONG-PASSWORD");
+        } else {
+          res.send("UNREGISTERED");
         }
-        else{
-            res.send('Inncorrect password')
-            console.log('inc')
-        }
+      }
     }
-    catch{
-        res.status(500).send()
-    }
+  );
 };
 
 export default LoginUser;
