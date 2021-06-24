9d4c55ad980bd5b01975cd33d7d48cdc1200a717
Rohan <rohanpatidat01@gmail.com>
2021-06-24 14:43:32 +0530

    landing page

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import Link from 'next/link';
 import styles from '../styles/Header.module.css';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import IconButton from '@material-ui/core/IconButton';
+import { useEffect } from 'react';
+import Accordion from "../components/Accordion";
+
+
 function landing() {
+    useEffect(() => {
+
+        var acc = document.getElementsByClassName('accordion');
+        var i = 0;
+        var len = acc.length;
+        for (i = 0; i < len; i++) {
+            acc[i].addEventListener('onClick', function () {
+                this.classList.toggle('active');
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + 'px';
+                }
+            })
+        }
+
+    }, [])
     return (
         <div className={styles.app}>
             <div className={styles.app__container}>
@@ -26,17 +49,38 @@ function landing() {
                             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio esse eius ipsam voluptatibus quod assumenda aliquid minima quaerat? Cupiditate repudiandae quibusdam quidem, temporibus quisquam sequi dolor repellat iste necessitatibus explicabo! Obcaecati nisi qui vitae eveniet.
                         </p>
                         <div className={styles.app__links}>
-
-
                             <Link href="/login"><button>LogIn</button></Link>
-
-
                             <Link href="/signup"><button>SignUp</button></Link>
-
-
                         </div>
                     </div>
+                </div>
+                <hr></hr>
+                <div className={styles.app__faqs}>
+                    <div className="___">
+                        <h2>Frequently Asked Questions(FAQs)</h2>
+                        <Accordion
+                            title="What is Lorem?"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        />
+                        <Accordion
+                            title="How do I change Lorem?"
+                            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        />
+                        <Accordion
+                            title="Can I have fans?"
+                            content="lorem ipsum dolor sit amet?"
+                        />
+                        <Accordion
+                            title="Can I have fans?"
+                            content="lorem ipsum dolor sit amet?"
+                        />
+                        <Accordion
+                            title="Can I have fans?"
+                            content="lorem ipsum dolor sit amet?"
+                        />
+                      
 
+                    </div>
                 </div>
                 <div className={styles.app__footer}>
                     <IconButton href="#"><InstagramIcon /></IconButton>
