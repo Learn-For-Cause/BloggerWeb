d21383bc8900b778995ad4b65a1191fbd8c5433c
Kevin D Goveas <kevin.goveas@cognitiveclouds.com>
2022-07-20 10:47:51 +0530

    temporary changes

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -3,27 +3,27 @@ import StarIcon from "@material-ui/icons/Star";
 import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Blogs = ({ id, data }) => {
   const img = "/images/header.jpg";
+  const router = useRouter();
   return (
-    <Link href="/posts/[id]" as={"/posts/" + id}>
-      <div className="posts">
-        <div className="posts__body">
-          <h1>{data.blogName}</h1>
-          <p>{data.blogDesc}</p>
-          <div className="posts__body__details">
-            <p>{data.blogWriter}</p>
-            <p>{data.blogTime}</p>
-            <p>{data.blogDate}</p>
-          </div>
+    <div className="posts" onClick={() => router.push(`/posts/${id}`)}>
+      <div className="posts__body">
+        <h1>{data.blogName}</h1>
+        <p>{data.blogDesc}</p>
+        <div className="posts__body__details">
+          <p>{data.blogWriter}</p>
+          <p>{data.blogTime}</p>
+          <p>{data.blogDate}</p>
         </div>
-        <div
-          className="posts__img"
-          style={{ backgroundImage: `url(${img})` }}
-        ></div>
       </div>
-    </Link>
+      <div
+        className="posts__img"
+        style={{ backgroundImage: `url(${img})` }}
+      ></div>
+    </div>
   );
 };
 
